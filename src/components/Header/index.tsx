80abfefebd2c198d3c8e6ed982e15f252734b3c3
devsalomao <christophersalomao5@gmail.com>
2025-07-02 16:33:00 -0300

    feat: componente Header criado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+// Esse código define um componente de cabeçalho do projeto
+function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <h1 className="text-2xl font-bold">Animal Shop</h1>
+    </header>
+  );
+}
+
+export default Header;
