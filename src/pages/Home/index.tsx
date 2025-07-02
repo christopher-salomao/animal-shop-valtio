ef7ccd12fbee3685513fe4718b1763b6046bc606
devsalomao <christophersalomao5@gmail.com>
2025-07-02 16:46:13 -0300

    feat: componente da pagina Home criado

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+// Página inicial do site, onde serão exibidos os produtos
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of our application.</p>
+    </div>
+  );
+}
+
+export default Home;
