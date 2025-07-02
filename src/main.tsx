0f2fd3523fd3958141efda35b0ca1927cbf671cf
devsalomao <christophersalomao5@gmail.com>
2025-07-02 17:15:33 -0300

    feat: react-query configurado

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import './index.css'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={routes} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes} />
+    </QueryClientProvider>
   </StrictMode>,
 )
