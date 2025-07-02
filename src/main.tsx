0f12b743ccb957573addf12ab7c57b53e28eaebd
devsalomao <christophersalomao5@gmail.com>
2025-07-02 17:17:53 -0300

    feat: toaster adicionado ao main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
 
 import './index.css'
 
@@ -12,6 +13,7 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster position="top-center" reverseOrder={false} />
       <RouterProvider router={routes} />
     </QueryClientProvider>
   </StrictMode>,
